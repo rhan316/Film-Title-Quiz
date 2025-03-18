c46fcc370e26ec117177f4fd2cf2d47803e8c57a
rhan316 <janek53@protonmail.com>
2025-03-18 14:53:39 +0100

    18.03.2025 15:00

--- src/main/java/org/example/filmtitlequiz/config/TmdbConfig.java
+++ src/main/java/org/example/filmtitlequiz/config/TmdbConfig.java
@@ -17,20 +17,4 @@ public class TmdbConfig {
 	@NotBlank(message = "URL cannot be blank")
 	@URL(message = "Invalid TMDB URL format")
 	private String url;
-
-	public String getApiKey() {
-		return apiKey;
-	}
-
-	public void setApiKey(String apiKey) {
-		this.apiKey = apiKey;
-	}
-
-	public String getUrl() {
-		return url;
-	}
-
-	public void setUrl(String url) {
-		this.url = url;
-	}
 }
